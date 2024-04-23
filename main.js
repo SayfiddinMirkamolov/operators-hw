7ad31352dc8cef8d28b305fee8786f9b8e112609
SayfiddinMirkamolov <sayfiddinmirkamolov7@gmail.com>
2024-04-23 07:19:26 +0500

    uyga vazifa

---
+++ main.js
@@ -0,0 +1,122 @@
+// 1- masala
+
+// let a, b ,y;
+// a= +prompt("a sonini kiriting :")
+// b= +prompt("b sonini kiriting :")
+
+// y=(-b/2) * (a**2 + b**2)
+
+// console.log("y="+y)
+
+// 2-masala
+
+// let a , b ,c , d , y;
+
+// a= +prompt("a sonini kiriting :")
+// b= +prompt("b sonini kiriting :")
+// c= +prompt("c sonini kiriting :")
+// d= +prompt("d sonini kiriting :")
+
+// y=((a+b)/(c+d))**2 + b**2
+// console.log("y="+y)
+
+//3-masala
+
+// let y,a,d,c;
+
+// a= +prompt("a sonini kiriting :")
+// d= +prompt("d sonini kiriting :")
+// c= +prompt("c sonini kiriting :")
+
+// y = 2*(a**d + (4*(c**2))/3)
+
+// console.log("y="+y)
+
+// 4- masala
+
+// let a, b, c, d, y
+
+// a = +prompt('a sonini kiriting :')
+// b = +prompt('b sonini kiriting :')
+// c = +prompt('c sonini kiriting :')
+// d = +prompt('d sonini kiriting :')
+
+// y = (1 / a ** 2) * (b / 10) ** 3 * (c + d) ** 2;
+
+// console.log("y="+y)
+
+// 5- masala  
+
+// let a , b ,c , d , y;
+
+// a= +prompt("a sonini kiriting :")
+// b= +prompt("b sonini kiriting :")
+// c= +prompt("c sonini kiriting :")
+// d= +prompt("d sonini kiriting :")
+
+// y= (((a/b)-1) / (c/(d-1)))**2
+
+// console.log("y="+y)
+
+// 6-masala
+
+// let a , b ,c , d , y;
+
+// a= +prompt("a sonini kiriting :")
+// b= +prompt("b sonini kiriting :")
+// c= +prompt("c sonini kiriting :")
+// d= +prompt("d sonini kiriting :")
+
+// y=((a+b)/(c+(d/(a+c))))
+
+// console.log("y="+y)
+
+// 7- masala
+
+// let a , b ,c  , y;
+
+// a= +prompt("a sonini kiriting :")
+// b= +prompt("b sonini kiriting :")
+// c= +prompt("c sonini kiriting :")
+
+
+// y=Math.sqrt((a+b+c)**2-(a-b-c)**2)
+
+// console.log("y="+y)
+
+// 8-masala
+
+// let a , b ,c  , y;
+
+// a= +prompt("a sonini kiriting :")
+// b= +prompt("b sonini kiriting :")
+// c= +prompt("c sonini kiriting :")
+
+// y= (a**2 + b**2 + c**2)/(a*b*c)
+
+// console.log("y="+y)
+
+// 9-masala
+
+// let a , b ,c , d , y;
+
+// a= +prompt("a sonini kiriting :")
+// b= +prompt("b sonini kiriting :")
+// c= +prompt("c sonini kiriting :")
+// d= +prompt("d sonini kiriting :")
+
+// y=a*((d**3)/3)+b*((c**2)/2)
+
+// console.log("y="+y)
+
+// 10 -masala
+
+// let a , b ,c  , y;
+
+// a= +prompt("a sonini kiriting :")
+// b= +prompt("b sonini kiriting :")
+// c= +prompt("c sonini kiriting :")
+
+// y=Math.abs(a/b - b/c) + Math.sqrt(Math.sqrt (a+c))
+
+// console.log("y="+y)
---
+++ readme.md
@@ -0,0 +1,26 @@
+JavaScript-da ma'lumot turlari, "type" operatori, asosiy turlar (primitive types), kompleks (manba va non-primitive) turlar, arifmetika (mix), unar operatorlar va matematika haqida quyidagi ma'lumotlarni o'z ichiga oladi:
+
+Ma'lumot turlari (Data Types):
+
+Primitive (Asosiy) turlar: JavaScript-da 7 ta asosiy ma'lumot turi mavjud:
+number: Raqamlar, o'nlik sonlar, butun sonlar, infinit (son bo'lmagan son), NaN (son emas) kabi qiymatlar.
+string: Matnlar, belgilar jadvali.
+boolean: To'g'ri (true) yoki yolg'on (false) qiymatlar.
+null: Qiymati yo'q ma'lumot turi.
+undefined: Definitsiya qilingan, lekin qiymati belgilanmagan ma'lumot turi.
+symbol: Unikal simvol qiymatlari.
+bigint: uzun sonlar yani ohirda  (10n) shu kabi tohtaydigan uzun sonlar ifodalash mumkin
+Complex (reference va non-primitive) turlar: JavaScript-da complex (manba va non-primitive) ma'lumot turlari ham mavjud:
+object: Ob'ektlar, jadval shaklidagi ma'lumotlarni saqlaydi.
+ 
+Type operators: JavaScript-da typeof operatori obyektni yoki qiymatning turi haqida ma'lumot beradi. Masalan, typeof x ifodasi x o'zgaruvchisining turi bilan natijani qaytaradi.
+
+ primitive types (Primitive Types): Raqam (number), matn (string), boolean (boolean), null, undefined va symbol turlarini ifodalaydi. Bu turlar muhim qiymatlarni ifodalash uchun ishlatiladi.
+
+complex (reference va non-primitive) turlar: Obyekt (object), massiv (array) va funksiya (function) kompleks yoki manba va non-primitive ma'lumot turlarini ifodalaydi. Bu turlar qo'shimcha ma'lumotlarni saqlash va umumiy operatsiyalarni bajarish uchun qo'llaniladi.
+
+Arifmetika (Mix): JavaScript-da arifmetik amallar (+, -, *, /, %) sonlar bilan ishlaydi. Bu amallar sonlar orasida yig'ish, ayirish, ko'paytirish, bo'lish va qoldiqni topishga yordam beradi.
+
+Unar operators: JavaScript-da "+" va "-" unar operatorlar sonlarni musbat yoki manfiy qilish uchun ishlatiladi.
+
+Math: JavaScript-da matematikaga oid funktsiyalar, masalan, Math.sqrt() (ildizni hisoblash), Math.pow() (darajaga oshirish), Math.abs() (modulni hisoblash) kabi amallar mavjud.
